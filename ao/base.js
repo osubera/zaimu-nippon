8dfc00ef161ad3353516a540979d3a01f1d1e97c
Shinichiro Tomizono <macomini@terigaki.local>
2016-04-16 06:32:14 +0900

    indent
    changes are white spaces only

--- ao/base.js
+++ ao/base.js
@@ -8,50 +8,50 @@ require kuro/base.js
 ############################*/
 
 define(['kuro/base'], function(Kuro_base){
-
-function Ao_base(){
   
-  /*############################
-  Nendo / this.nendo
-  年度変数のコンストラクタ
-  ############################*/
-  
-  function Nendo(year, begin, end){
-    var _year = new Kuro_base.number;
-    var _begin = new Kuro_base.date;
-    var _end = new Kuro_base.date;
-    var _month = new Kuro_base.number;
+  function Ao_base(){
+    
+    /*############################
+    Nendo / this.nendo
+    年度変数のコンストラクタ
+    ############################*/
+    
+    function Nendo(year, begin, end){
+      var _year = new Kuro_base.number;
+      var _begin = new Kuro_base.date;
+      var _end = new Kuro_base.date;
+      var _month = new Kuro_base.number;
+      
+      Object.defineProperties(this, {
+        "year": { get: function(){ return _year; },
+                     set: function(value){ _year = parseYear(this, value); },
+                     configurable: true },
+        "defaultYear": { value: new Date().getFullYear(), writable: true, configurable: true }
+      });
+      
+      this.begin = begin;
+      this.end = end;
+      this.year = year;
+      
+      function parseYear(obj, x) {
+        var y = Number.parseInt(x);
+        if(y == 0 || y.toString() == "NaN") { y = obj.defaultYear; }
+        if(!obj.begin) {}
+        if(!obj.end) {}
+        if(!obj.month) {}
+        return(y);
+      }
+    }
+    this.nendo = Nendo;
+    
+    /*############################
+    properties
+    ############################*/
     
     Object.defineProperties(this, {
-      "year": { get: function(){ return _year; },
-                   set: function(value){ _year = parseYear(this, value); },
-                   configurable: true },
-      "defaultYear": { value: new Date().getFullYear(), writable: true, configurable: true }
+      "xxxnendo": { value: 3}//new Nendo }
     });
-    
-    this.begin = begin;
-    this.end = end;
-    this.year = year;
-    
-    function parseYear(obj, x) {
-      var y = Number.parseInt(x);
-      if(y == 0 || y.toString() == "NaN") { y = obj.defaultYear; }
-      if(!obj.begin) {}
-      if(!obj.end) {}
-      if(!obj.month) {}
-      return(y);
-    }
   }
-  this.nendo = Nendo;
   
-  /*############################
-  properties
-  ############################*/
-  
-  Object.defineProperties(this, {
-    "xxxnendo": { value: 3}//new Nendo }
-  });
-}
-
-return(Ao_base);
+  return(Ao_base);
 });
