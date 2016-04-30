14b6229f4a70ba2c1e45354ec91397c58724f6ef
Shinichiro Tomizono <macomini@terigaki.local>
2016-05-01 04:32:17 +0900

    adjust white spaces and position

--- kuro/event.js
+++ kuro/event.js
@@ -16,47 +16,45 @@ define(function(){
     イベント生成のコンストラクタ
     ############################*/
     
-
-function Event(name){
-  this.name = name;
-  this.callbacks = [];
-  this.data = [];
-}
-Event.prototype.registerCallback = function(callback, data){
-  this.callbacks.push(callback);
-  this.data.push(data);
-}
-this.event = Event;
+    function Event(name){
+      this.name = name;
+      this.callbacks = [];
+      this.data = [];
+    }
+    this.event = Event;
+    
+    Event.prototype.registerCallback = function(callback, data){
+      this.callbacks.push(callback);
+      this.data.push(data);
+    }
     
     /*############################
      Reactor / this.reactor
     イベント管理のコンストラクタ
     ############################*/
     
-function Reactor(){
-  this.events = {};
-}
-this.reactor = Reactor;
-
-Reactor.prototype.registerEvent = function(eventName){
-  var event = new Event(eventName);
-  this.events[eventName] = event;
-};
-
-Reactor.prototype.dispatchEvent = function(eventName, eventArgs){
-  var event = this.events[eventName];
-  var callbacks = event.callbacks;
-  var data = event.data;
-  for(var i = 0; i < callbacks.length; i++) {
-    callbacks[i](eventArgs, data[i]);
-  }
-};
-
-Reactor.prototype.addEventListener = function(eventName, callback, data){
-  this.events[eventName].registerCallback(callback, data);
-};
-
-
+    function Reactor(){
+      this.events = {};
+    }
+    this.reactor = Reactor;
+    
+    Reactor.prototype.registerEvent = function(eventName){
+      var event = new Event(eventName);
+      this.events[eventName] = event;
+    };
+    
+    Reactor.prototype.dispatchEvent = function(eventName, eventArgs){
+      var event = this.events[eventName];
+      var callbacks = event.callbacks;
+      var data = event.data;
+      for(var i = 0; i < callbacks.length; i++) {
+        callbacks[i](eventArgs, data[i]);
+      }
+    };
+    
+    Reactor.prototype.addEventListener = function(eventName, callback, data){
+      this.events[eventName].registerCallback(callback, data);
+    };
   };
   
   return(Kuro_event);
